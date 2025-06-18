985a34e22268daee68b91644a0024a8bf1c647fb
nakamarusun <jason@jasoncoding.com>
2025-06-18 16:33:03 +0700

    Added terminal for chirpstack

---
+++ frontend/src/components/ChirpstackStream.tsx
@@ -0,0 +1,49 @@
+import { use, useEffect, useRef, useState } from 'react'
+import config from '../config';
+import { EventSource } from 'eventsource';
+import useAuth from '../hooks/useAuth';
+
+export default function ChirpstackStream() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const auth = useAuth();
+
+  const chirpstackLogsRef = useRef<HTMLPreElement>(null);
+
+  useEffect(() => {
+    if (chirpstackLogsRef.current) {
+      chirpstackLogsRef.current.scrollTo(0, chirpstackLogsRef.current.scrollHeight);
+    }
+  }, [messages]);
+
+  // Use SSE to connect to Chirpstack logs
+  useEffect(() => {
+    const sse = new EventSource(
+      `${config.baseUrl}/events/packets`,
+      {
+        fetch: (input, init) => (
+          fetch(input, {
+            ...init,
+            headers: {
+              ...init.headers,
+              Authorization: `Bearer ${auth.token}`,
+            },
+          })
+
+        ),
+      }
+    );
+    sse.addEventListener("message", (event) => {
+      setMessages((prev) => [...prev, event.data]);
+      // Here you can handle the data, e.g., update state or log it
+    });
+
+    return () => {
+      sse.close();
+    }
+  }, []);
+  return (
+    <pre ref={chirpstackLogsRef} className="h-full overflow-y-auto px-2 whitespace-pre-wrap">
+      {messages.join("\n")}
+    </pre>
+  )
+}
--- frontend/src/components/Terminal.tsx
+++ frontend/src/components/Terminal.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import SerialConnection from './SerialConnection';
 import useSerial from '../hooks/useSerial';
 import clsx from 'clsx';
+import gwPic from '../assets/station-minimalistic-svgrepo-com.svg';
+import ChirpstackStream from './ChirpstackStream';
 
 function trimValueFromAt(value: string): string {
   return value.split("\n")[0].trim();
@@ -18,6 +20,14 @@ export default function Terminal() {
 
   const [termLog, setTermLog] = useState<string[]>([]);
 
+  const serialLogsRef = useRef<HTMLPreElement>(null);
+
+  useEffect(() => {
+    if (serialLogsRef.current) {
+      serialLogsRef.current.scrollTop = serialLogsRef.current.scrollHeight;
+    }
+  }, [termLog])
+
   // Do some preliminary data collection
   useEffect(() => {
     if (serial.state === "disconnected") return;
@@ -28,18 +38,18 @@ export default function Terminal() {
       setAppSKey(trimValueFromAt(await serial.sendAndWait("AT+APPSKEY=?", 1000)));
     })();
 
+    function addToLog(data: string, addStr: string="") {
+      const timestamp = (new Date()).toISOString().replace("T", " ").replace("Z", "");
+      data = `[${timestamp}]${addStr}${data}`;
+      setTermLog((prev) => [...prev, data]);      
+    }
+
     // Register some uhhh callbacks cuh
     serial.onDataReceived((data) => {
-      // Timestamp the data
-      const timestamp = (new Date()).toISOString().replace("T", " ").replace("Z", "");
-      data = `[${timestamp}]${data}`;
-      setTermLog((prev) => [...prev, data]);
+      addToLog(data);
     });
     serial.onDataSent((data) => {
-      // Timestamp the data
-      const timestamp = (new Date()).toISOString().replace("T", " ").replace("Z", "");
-      data = `[${timestamp}]>${data}`;
-      setTermLog((prev) => [...prev, data]);
+      addToLog(data, " > ");
     });
   }, [serial.state]);
 
@@ -55,26 +65,24 @@ export default function Terminal() {
   return (
     <div className="font-source-code-pro text-xs md:text-sm flex flex-col text-white h-full w-full font-normal relative">
       {/* Main screen */}
-      <div className={clsx("flex-grow flex flex-col text-left", {
+      <div className={clsx("flex-grow flex flex-col text-left overflow-auto", {
         "justify-center items-center": serial.state === "disconnected",
       })}>
       {serial.state === "connected" ? (
-        <div className="flex flex-col h-full">
-          <div className="flex flex-col flex-grow">
+        <div className="flex flex-col h-full overflow-auto">
+          <div className="flex flex-col flex-grow h-full overflow-auto">
             <p className="font-semibold bg-cyan-600 px-2">
               Serial Term
             </p>
-            <pre className="overflow-y-auto px-2">
+            <pre ref={serialLogsRef} className="overflow-y-auto px-2 whitespace-pre-wrap">
               {termLog.join("\n")}
             </pre>
           </div>
-          <div className="flex flex-col flex-grow">
+          <div className="flex flex-col flex-grow h-full overflow-auto">
             <p className="font-semibold bg-cyan-600 px-2">
-              Gateways RAW
+              Chirpstack Logs
             </p>
-            <pre className="overflow-y-auto px-2">
-              {termLog.join("\n")}
-            </pre>
+              <ChirpstackStream />
           </div>
         </div>
       ) : (
@@ -83,18 +91,45 @@ export default function Terminal() {
       </div>
 
       {/* Bottom long text */}
-      <div className="bg-gray-400 text-black py-0.5 pb-3 flex flex-col md:flex-row items-start md:items-center md:justify-between px-2">
-        <div>
-          <span className="mr-2">DevAddr:</span>
-          <span>{devAddr}</span>
+      <div className="flex flex-col">
+        <div className="bg-blue-600 flex flex-row items-center">
+          <div className="flex flex-row flex-grow text-left">
+            <div className="font-semibold pl-2">
+              <p>Last Conn</p>
+              <p>-6.32, 553.2</p>
+              <p>05:12:22</p>
+            </div>
+            <div className="w-3" />
+            <div>
+              <p>Downlink</p>
+              <p>DL RSSI: 55</p>
+              <p>DL SNR: 55</p>
+            </div>
+            <div className="w-3" />
+            <div>
+              <p>gw-05321233</p>
+              <p>RSSI: 55</p>
+              <p>SNR: 55</p>
+            </div>
+          </div>
+          <div className="flex flex-row items-center gap-2 bg-fuchsia-600 h-full">
+            <img src={gwPic} alt="Station Minimalistic Logo" className="w-10 h-10 p-1" />
+            <p className="text-3xl font-bold mr-2">0</p>
+          </div>
         </div>
-        <div>
-          <span className="mr-2">NwkSKey:</span>
-          <span>{nwkSKey}</span>
-        </div>
-        <div>
-          <span className="mr-2">AppSKey:</span>
-          <span>{appSKey}</span>
+        <div className="bg-green-500 text-black py-0.5 md:pb-3 flex flex-col md:flex-row items-start md:items-center md:justify-between px-2">
+          <div>
+            <span className="mr-2">DevAddr:</span>
+            <span>{devAddr}</span>
+          </div>
+          <div>
+            <span className="mr-2">NwkSKey:</span>
+            <span>{nwkSKey}</span>
+          </div>
+          <div>
+            <span className="mr-2">AppSKey:</span>
+            <span>{appSKey}</span>
+          </div>
         </div>
       </div>
     </div>
