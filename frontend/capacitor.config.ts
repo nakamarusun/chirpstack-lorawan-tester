9de5460682f958b47b4a0b8c88da737dda50b8b4
nakamarusun <jason@jasoncoding.com>
2025-06-23 15:56:02 +0700

    successfully installed capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jasoncoding.chirploratest',
+  appName: 'Chirpstack LoRaWAN Tester',
+  webDir: 'dist'
+};
+
+export default config;
